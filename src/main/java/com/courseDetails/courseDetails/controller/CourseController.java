4b0d89defaf4bd8090c27fbd42bd301f1596afe8
Shreeram <shreeram0102@gmail.com>
2024-03-15 22:16:42 +0530

    MobileService Added

--- src/main/java/com/courseDetails/courseDetails/controller/CourseController.java
+++ src/main/java/com/courseDetails/courseDetails/controller/CourseController.java
@@ -1,17 +1,19 @@
 package com.courseDetails.courseDetails.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/course")
 public class CourseController {
 
-    @GetMapping(value = "/name")
+    @GetMapping(value = "/name/{name}")
     public String jenkinDeployStatus(@PathVariable String name){
         return "First Jenkins deployment successful by " + name;
+    }
+
+    @GetMapping(value = "/getMobile")
+    public String getMobile(@RequestParam String model){
+        return "Deployemnt of Mobile package successful" + model;
     }
 
 }
