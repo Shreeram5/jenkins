d4f2954b2018d0cfc8bd5d1892b5c573d144d055
Shreeram <shreeram0102@gmail.com>
2024-03-15 20:58:10 +0530

    success commit

--- src/test/java/com/courseDetails/courseDetails/controller/CourseControllerTest.java
+++ src/test/java/com/courseDetails/courseDetails/controller/CourseControllerTest.java
@@ -3,8 +3,12 @@ package com.courseDetails.courseDetails.controller;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootTest
 public class CourseControllerTest {
@@ -12,11 +16,15 @@ public class CourseControllerTest {
     @InjectMocks
     private CourseController courseController;
 
+    @Mock
+    private HttpServletRequest httpServlet;
+
     @Test
     public void jenkinDeployStatusTest(){
-       String message = Mockito.when(courseController.jenkinDeployStatus("test"))
-                .thenReturn("deployed successsful").toString();
-        Assertions.assertEquals(message, "deployed successsful");
+       String actualMessage = "First Jenkins deployment successful by John";
+       Mockito.when(httpServlet.getRequestURI()).thenReturn("/name");
+       String expextedoutput = courseController.jenkinDeployStatus("John");
+       Assertions.assertEquals(actualMessage, expextedoutput);
     }
 
 }
